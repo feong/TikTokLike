c1b764623c80f29ba2b3f99d065593569eed6af9
Xiaofeng Liu <xiaofeng@blacklake.cn>
2020-04-23 10:14:38 +0800

    📦 add module resolver plugin

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
-import {RootViews} from './app/views/root/RootViews';
+import {RootViews} from 'app/views/root/RootViews';
 
 // declare const global: {HermesInternal: null | {}};
 
