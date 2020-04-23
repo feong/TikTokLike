0cb1bfd2684f4ff8df5d4332a120e7565f70e6e8
Xiaofeng Liu <xiaofeng@blacklake.cn>
2020-04-24 01:53:02 +0800

    📦 change asyncstorage lib

--- app/utils/postComment.ts
+++ app/utils/postComment.ts
@@ -1,4 +1,4 @@
-import {AsyncStorage} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import {iComment} from './dataType';
 
 export const FAKE_COMMENT_KEY = 'FAKECOMMENTKEY';
