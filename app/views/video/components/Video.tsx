4fe2cafef32e179a7014e69d963eecc54139dc12
Xiaofeng Liu <xiaofeng@blacklake.cn>
2020-04-30 15:14:19 +0800

    👌 video component support double tap

--- app/views/video/components/Video.tsx
+++ app/views/video/components/Video.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
 import RNVideo from 'react-native-video';
-import {StyleSheet, TouchableWithoutFeedback} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {Colors} from 'react-native-paper';
+import {
+  TapGestureHandler,
+  TapGestureHandlerStateChangeEvent,
+  State,
+} from 'react-native-gesture-handler';
 
 interface VideoProps {
   uri: string;
+  onDoubleTap?: (point: {x: number; y: number}) => void;
   paused?: boolean;
   onPausedChanged?: (paused: boolean) => void;
 }
 
-export const Video: React.FC<VideoProps> = (props) => {
+export const Video = React.memo((props: VideoProps) => {
   const {
     uri = 'https://cloud.video.taobao.com/play/u/516544213/p/2/e/6/t/1/250707633677.mp4',
+    onDoubleTap,
     onPausedChanged,
   } = props;
   const [paused, setPaused] = React.useState(props.paused);
@@ -19,28 +26,45 @@ export const Video: React.FC<VideoProps> = (props) => {
   React.useEffect(() => {
     setPaused(props.paused);
   }, [props.paused]);
+  const doubleTap = React.useRef(React.createRef<TapGestureHandler>().current);
 
-  const onVideoPress = React.useCallback(() => {
-    const nextState = !paused;
-    setPaused(nextState);
-    onPausedChanged && onPausedChanged(nextState);
-  }, [onPausedChanged, paused]);
+  const onSingleTapped = (event: TapGestureHandlerStateChangeEvent) => {
+    if (event.nativeEvent.state === State.ACTIVE) {
+      const nextState = !paused;
+      setPaused(nextState);
+      onPausedChanged && onPausedChanged(nextState);
+    }
+  };
+
+  const onDoubleTapped = (event: TapGestureHandlerStateChangeEvent) => {
+    if (event.nativeEvent.state === State.ACTIVE) {
+      onDoubleTap &&
+        onDoubleTap({x: event.nativeEvent.x, y: event.nativeEvent.y});
+    }
+  };
 
   return (
-    <TouchableWithoutFeedback onPress={onVideoPress}>
-      <RNVideo
-        repeat
-        paused={paused}
-        source={{uri}}
-        style={styles.backgroundVideo}
-      />
-    </TouchableWithoutFeedback>
+    <TapGestureHandler
+      waitFor={doubleTap}
+      onHandlerStateChange={onSingleTapped}>
+      <TapGestureHandler
+        ref={doubleTap}
+        onHandlerStateChange={onDoubleTapped}
+        numberOfTaps={2}>
+        <View style={styles.backgroundVideo}>
+          <RNVideo repeat paused={paused} source={{uri}} style={styles.video} />
+        </View>
+      </TapGestureHandler>
+    </TapGestureHandler>
   );
-};
+});
 
 const styles = StyleSheet.create({
   backgroundVideo: {
     flex: 1,
     backgroundColor: Colors.black,
+  },
+  video: {
+    flex: 1,
   },
 });
